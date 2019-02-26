2eea60fbcac0a79011917b2f1b66f254ba832360
ramosjm <jjramos_2004@hotmail.com>
2019-02-26 15:09:24 +0000

    added planets to view.

--- src/views/planet_info_view.js
+++ src/views/planet_info_view.js
@@ -13,10 +13,20 @@ PlanetInfo.prototype.bindEvents = function (){
 };
 
 PlanetInfo.prototype.outputDetails = function(planetDets){
-  const detail = document.createElement('p');
-  console.log(this.container);
-  detail.textContent = planetDets.name;
+  const detail = document.createElement('li');
+  const name = document.createElement('h2');
+  name.textContent = planetDets.name;
+  detail.appendChild(name);
+  const orbit = document.createElement('h3');
+  orbit.textContent = planetDets.orbit;
+  detail.appendChild(orbit);
+  const surfaceArea = document.createElement('h4');
+  surfaceArea.textContent = planetDets.surfaceArea;
+  detail.appendChild(surfaceArea);
+  this.container.innerHTML = '';
   this.container.appendChild(detail);
 };
+
+PlanetInfo.prototype
 
 module.exports = PlanetInfo;
